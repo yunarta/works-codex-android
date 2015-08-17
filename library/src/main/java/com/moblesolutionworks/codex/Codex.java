51be4e5e433ce6ff5bfbab535db8eafafb345a3c
Yunarta Kartawahyudi <yunarta.kartawahyudi@gmail.com>
2015-08-17 19:25:20 +0800

    major changes due to TreeSet uses comparator for identifying uniqueness of action hook, causing hook with same priority and args clashes

--- library/src/main/java/com/moblesolutionworks/codex/Codex.java
+++ library/src/main/java/com/moblesolutionworks/codex/Codex.java
@@ -1,52 +1,142 @@
 package com.moblesolutionworks.codex;
 
+import android.util.Log;
 import android.util.SparseArray;
 
 import java.lang.reflect.InvocationTargetException;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Created by yunarta on 9/8/15.
  */
-public class Codex
-{
-    SparseArray<Set<ActionHookHandler>> allHooks = new SparseArray<>();
+public class Codex {
+    SparseArray<List<ActionHookHandler>>        allHooks       = new SparseArray<>();
+    SparseArray<List<PropertySubscriberHandler>> allSubscribers = new SparseArray<>();
+    SparseArray<DefaultPropertyHandler>         allProperties  = new SparseArray<>();
 
-    public void register(Object object)
-    {
-        SparseArray<Set<ActionHookHandler>> hooks = ReflectionAnnotationProcessor.findActionHooks(object);
+    public void register(Object object) {
+        SparseArray<List<ActionHookHandler>> hooks = ReflectionAnnotationProcessor.findActionHooks(object);
 
+        // register all hooks
         int length = hooks.size();
-        for (int i = 0; i < length; i++)
-        {
+        for (int i = 0; i < length; i++) {
             int key = hooks.keyAt(i);
-            Set<ActionHookHandler> registeredHooks = allHooks.get(key, new TreeSet<ActionHookHandler>());
+            List<ActionHookHandler> registeredHooks = allHooks.get(key, new ArrayList<ActionHookHandler>());
 
-            Set<ActionHookHandler> handlers = hooks.valueAt(i);
+            List<ActionHookHandler> handlers = hooks.valueAt(i);
+
             registeredHooks.addAll(handlers);
+            Collections.sort(registeredHooks);
 
             allHooks.put(key, registeredHooks);
+        }
+
+        // check if the new object has property, and dispatch to existing property subscriber
+        SparseArray<DefaultPropertyHandler> properties = ReflectionAnnotationProcessor.findDefaultProperties(object);
+        if (properties != null && properties.size() != 0) {
+            length = properties.size();
+
+            for (int i = 0; i < length; i++) {
+                int key = properties.keyAt(i);
+                DefaultPropertyHandler handler = properties.valueAt(i);
+                allProperties.put(key, handler);
+
+                Object value;
+                try {
+                    value = handler.getProperty();
+                } catch (InvocationTargetException e) {
+                    throw new RuntimeException(e);
+                }
+
+                dispatchPropertyChange(value, allSubscribers.get(key));
+            }
+        }
+
+        // now get all property subscriber of the object
+        SparseArray<List<PropertySubscriberHandler>> subscribers = ReflectionAnnotationProcessor.findPropertySubscribers(object);
+        if (subscribers != null && subscribers.size() != 0) {
+            length = subscribers.size();
+
+            for (int i = 0; i < length; i++) {
+                int key = subscribers.keyAt(i);
+                List<PropertySubscriberHandler> handlers = subscribers.valueAt(i);
+
+                List<PropertySubscriberHandler> registeredHandlers = allSubscribers.get(key, new ArrayList<PropertySubscriberHandler>());
+                registeredHandlers.addAll(handlers);
+            }
+        }
+
+        // dispatch the properties to newly registered subscribers
+        if (subscribers != null && subscribers.size() != 0) {
+            for (int i = 0; i < length; i++) {
+                int key = subscribers.keyAt(i);
+                List<PropertySubscriberHandler> handlers = subscribers.valueAt(i);
+
+                DefaultPropertyHandler propertyHandler = allProperties.get(key);
+                if (propertyHandler == null) return;
+
+                Object value;
+                try {
+                    value = propertyHandler.getProperty();
+                } catch (InvocationTargetException e) {
+                    throw new RuntimeException(e);
+                }
+
+                dispatchPropertyChange(value, handlers);
+            }
         }
     }
 
     /**
-     * Public an action to the system and allow the hook to pick it up.
+     * Publish an action to the system and allow the hook to pick it up.
      */
-    public void startAction(String name, Object... args)
-    {
+    public void startAction(String name, Object... args) {
         int key = (name + args.length).hashCode();
-        Set<ActionHookHandler> handlers = allHooks.get(key);
+        List<ActionHookHandler> handlers = allHooks.get(key);
         if (handlers == null) return;
 
-        for (ActionHookHandler handler : handlers)
-        {
-            try
-            {
+        ArrayList<ActionHookHandler> executingHandlers = new ArrayList<>(handlers);
+        for (ActionHookHandler handler : executingHandlers) {
+            try {
                 handler.actionHook(args);
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
             }
-            catch (InvocationTargetException e)
-            {
+        }
+    }
+
+    /**
+     * Update specified property for this owner
+     */
+    public void updateProperty(Object owner, String name) {
+        int key = name.hashCode();
+
+        DefaultPropertyHandler propertyHandler = allProperties.get(key);
+        if (propertyHandler == null) return;
+
+        if (propertyHandler.target != owner) {
+            throw new IllegalStateException("only owner can dispatch this property change");
+        }
+
+        Object value;
+        try {
+            value = propertyHandler.getProperty();
+        } catch (InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
+
+        dispatchPropertyChange(value, allSubscribers.get(key));
+    }
+
+    private void dispatchPropertyChange(Object value, List<PropertySubscriberHandler> handlers) {
+        if (handlers == null) return;
+
+        for (PropertySubscriberHandler handler : handlers) {
+            try {
+                handler.receiveProperty(value);
+            } catch (InvocationTargetException e) {
                 e.printStackTrace();
             }
         }
--- library/src/main/java/com/moblesolutionworks/codex/ReflectionAnnotationProcessor.java
+++ library/src/main/java/com/moblesolutionworks/codex/ReflectionAnnotationProcessor.java
@@ -5,28 +5,25 @@ import android.util.SparseArray;
 
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
-import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
-import java.util.Set;
 
 /**
  * Created by yunarta on 9/8/15.
  */
-public class ReflectionAnnotationProcessor
-{
-    static final SparseArray<SparseArray<Set<MethodInfo>>> allActionHooks;
-    static final SparseArray<SparseArray<Set<MethodInfo>>> allPropertyHooks;
-    static final SparseArray<SparseArray<MethodInfo>> allDefaultProperties;
+public class ReflectionAnnotationProcessor {
+    static final SparseArray<SparseArray<List<MethodInfo>>> allActionHooks;
+    static final SparseArray<SparseArray<List<MethodInfo>>> allPropertySubscribers;
+    static final SparseArray<SparseArray<MethodInfo>>       allDefaultProperties;
 
-    static
-    {
+    static {
         allActionHooks = new SparseArray<>();
-        allPropertyHooks = new SparseArray<>();
+        allPropertySubscribers = new SparseArray<>();
         allDefaultProperties = new SparseArray<>();
     }
 
-    static class MethodInfo implements Comparable<MethodInfo>
-    {
+    static class MethodInfo implements Comparable<MethodInfo> {
         final int name;
 
         final int priority;
@@ -41,24 +38,20 @@ public class ReflectionAnnotationProcessor
 
         int hashCode;
 
-        public MethodInfo(ActionHook annotation, Method method)
-        {
+        public MethodInfo(ActionHook annotation, Method method) {
             this(annotation.name().hashCode(), annotation.priority(), false, method);
         }
 
 
-        public MethodInfo(PropertyHook annotation, Method method)
-        {
+        public MethodInfo(PropertySubscriber annotation, Method method) {
             this(annotation.name().hashCode(), 0, false, method);
         }
 
-        public MethodInfo(DefaultProperty annotation, Method method)
-        {
+        public MethodInfo(DefaultProperty annotation, Method method) {
             this(annotation.name().hashCode(), 0, annotation.cached(), method);
         }
 
-        private MethodInfo(int name, int priority, boolean cached, Method method)
-        {
+        private MethodInfo(int name, int priority, boolean cached, Method method) {
             this.name = name;
             this.priority = priority;
             this.cached = cached;
@@ -81,14 +74,12 @@ public class ReflectionAnnotationProcessor
         }
 
         @Override
-        public int hashCode()
-        {
+        public int hashCode() {
             return hashCode;
         }
 
         @Override
-        public boolean equals(Object o)
-        {
+        public boolean equals(Object o) {
             if (this == o) return true;
             if (o == null || getClass() != o.getClass()) return false;
 
@@ -97,26 +88,21 @@ public class ReflectionAnnotationProcessor
         }
 
         @Override
-        public int compareTo(MethodInfo another)
-        {
+        public int compareTo(MethodInfo another) {
             return priority < another.priority ? -1 : (priority == another.priority ? 0 : 1);
         }
     }
 
-    public static void process(Class<?> cl)
-    {
+    public static void process(Class<?> cl) {
         Class<?> examined = cl;
 
-        do
-        {
-            SparseArray<Set<MethodInfo>> classActionHookMethods = new SparseArray<>();
-            SparseArray<Set<MethodInfo>> classPropertyHookMethods = new SparseArray<>();
+        do {
+            SparseArray<List<MethodInfo>> classActionHookMethods = new SparseArray<>();
+            SparseArray<List<MethodInfo>> classPropertySubscriberMethods = new SparseArray<>();
             SparseArray<MethodInfo> classDefaultPropertyMethods = new SparseArray<>();
 
-            for (Method method : examined.getDeclaredMethods())
-            {
-                if (method.isBridge())
-                {
+            for (Method method : examined.getDeclaredMethods()) {
+                if (method.isBridge()) {
                     // The compiler sometimes creates synthetic bridge methods as part of the
                     // type erasure process. As of JDK8 these methods now include the same
                     // annotations as the original declarations. They should be ignored for
@@ -124,13 +110,11 @@ public class ReflectionAnnotationProcessor
                     continue;
                 }
 
-                if (method.isAnnotationPresent(ActionHook.class))
-                {
+                if (method.isAnnotationPresent(ActionHook.class)) {
                     ActionHook hook = method.getAnnotation(ActionHook.class);
 
                     // method has to be public
-                    if ((method.getModifiers() & Modifier.PUBLIC) == 0)
-                    {
+                    if ((method.getModifiers() & Modifier.PUBLIC) == 0) {
                         throw new IllegalArgumentException(
                                 String.format(Locale.ENGLISH,
                                         "Method %s has @ActionHook annotation but is not public", method)
@@ -140,109 +124,150 @@ public class ReflectionAnnotationProcessor
                     Class<?>[] params = method.getParameterTypes();
 
                     int name = (hook.name() + params.length).hashCode();
-                    Set<MethodInfo> methods = classActionHookMethods.get(name);
-                    if (methods == null)
-                    {
-                        methods = new HashSet<>();
+                    List<MethodInfo> methods = classActionHookMethods.get(name);
+                    if (methods == null) {
+                        methods = new ArrayList<>();
                         classActionHookMethods.put(name, methods);
                     }
 
                     methods.add(new MethodInfo(hook, method));
-                }
-                else if (method.isAnnotationPresent(PropertyHook.class))
-                {
-                    PropertyHook hook = method.getAnnotation(PropertyHook.class);
+                } else if (method.isAnnotationPresent(PropertySubscriber.class)) {
+                    PropertySubscriber subscriber = method.getAnnotation(PropertySubscriber.class);
 
                     Class<?>[] params = method.getParameterTypes();
-                    if ((params.length != 1) || ((method.getModifiers() & Modifier.PUBLIC) == 0))
-                    {
+                    if ((params.length != 1) || ((method.getModifiers() & Modifier.PUBLIC) == 0)) {
                         throw new IllegalArgumentException(
                                 String.format(Locale.ENGLISH,
-                                        "Method %s has @PropertyHook annotation but has too many argumets", method)
+                                        "Method %s has @PropertySubscriber annotation but has too many argumets", method)
                         );
                     }
 
-                    int name = hook.name().hashCode();
-                    Set<MethodInfo> methods = classPropertyHookMethods.get(name);
-                    if (methods == null)
-                    {
-                        methods = new HashSet<>();
-                        classPropertyHookMethods.put(name, methods);
+                    int name = subscriber.name().hashCode();
+                    List<MethodInfo> methods = classPropertySubscriberMethods.get(name);
+                    if (methods == null) {
+                        methods = new ArrayList<>();
+                        classPropertySubscriberMethods.put(name, methods);
                     }
 
-                    methods.add(new MethodInfo(hook, method));
-                }
-                else if (method.isAnnotationPresent(DefaultProperty.class))
-                {
-                    DefaultProperty hook = method.getAnnotation(DefaultProperty.class);
+                    methods.add(new MethodInfo(subscriber, method));
+                } else if (method.isAnnotationPresent(DefaultProperty.class)) {
+                    DefaultProperty property = method.getAnnotation(DefaultProperty.class);
 
                     Class<?>[] params = method.getParameterTypes();
                     Class<?> returnType = method.getReturnType();
 
-                    if ((params.length != 0) || ((method.getModifiers() & Modifier.PUBLIC) == 0) || returnType.equals(Void.TYPE))
-                    {
+                    if ((params.length != 0) || ((method.getModifiers() & Modifier.PUBLIC) == 0) || returnType.equals(Void.TYPE)) {
                         throw new IllegalArgumentException(
                                 String.format(Locale.ENGLISH,
                                         "Method %s has @DefaultProperty annotation but has argumets", method)
                         );
                     }
 
-                    int name = hook.name().hashCode();
+                    int name = property.name().hashCode();
                     MethodInfo methodInfo = classDefaultPropertyMethods.get(name);
-                    if (methodInfo != null)
-                    {
+                    if (methodInfo != null) {
                         throw new IllegalArgumentException(
                                 String.format(Locale.ENGLISH,
-                                        "Method %s has already been registered", hook.name())
+                                        "Method %s has already been registered", property.name())
                         );
                     }
 
-                    classDefaultPropertyMethods.put(name, new MethodInfo(hook, method));
+                    classDefaultPropertyMethods.put(name, new MethodInfo(property, method));
                 }
             }
 
             allActionHooks.put(examined.hashCode(), classActionHookMethods);
-            allPropertyHooks.put(examined.hashCode(), classPropertyHookMethods);
+            allPropertySubscribers.put(examined.hashCode(), classPropertySubscriberMethods);
             allDefaultProperties.put(examined.hashCode(), classDefaultPropertyMethods);
 
-            if (examined.isAnnotationPresent(InheritCodex.class))
-            {
+            if (examined.isAnnotationPresent(InheritCodex.class)) {
                 examined = examined.getSuperclass();
-            }
-            else
-            {
+            } else {
                 examined = null;
             }
         }
         while (examined != null);
     }
 
-    public static SparseArray<Set<ActionHookHandler>> findActionHooks(Object object)
-    {
+    public static SparseArray<List<ActionHookHandler>> findActionHooks(Object object) {
         Class<?> cl = object.getClass();
 
-        SparseArray<Set<ActionHookHandler>> actionHookHandlers = new SparseArray<>();
+        SparseArray<List<ActionHookHandler>> actionHookHandlers = new SparseArray<>();
 
-        SparseArray<Set<MethodInfo>> actionHooks = allActionHooks.get(cl.hashCode());
-        if (actionHooks == null)
-        {
+        SparseArray<List<MethodInfo>> actionHooks = allActionHooks.get(cl.hashCode());
+        if (actionHooks == null) {
             process(cl);
             actionHooks = allActionHooks.get(cl.hashCode());
         }
 
         int length = actionHooks.size();
-        if (length != 0)
-        {
-            for (int i = 0; i < length; i++)
-            {
+        if (length != 0) {
+            for (int i = 0; i < length; i++) {
                 int key = actionHooks.keyAt(i);
 
-                Set<MethodInfo> methodInfos = actionHooks.valueAt(i);
-                Set<ActionHookHandler> handlers = new HashSet<>();
+                List<MethodInfo> methodInfos = actionHooks.valueAt(i);
+                List<ActionHookHandler> handlers = new ArrayList<>();
 
-                for (MethodInfo methodInfo : methodInfos)
-                {
+                for (MethodInfo methodInfo : methodInfos) {
                     ActionHookHandler handler = new ActionHookHandler(object, methodInfo);
+                    handlers.add(handler);
+                }
+
+                actionHookHandlers.put(key, handlers);
+            }
+        }
+
+        return actionHookHandlers;
+    }
+
+    public static SparseArray<DefaultPropertyHandler> findDefaultProperties(Object object) {
+        Class<?> cl = object.getClass();
+
+        SparseArray<DefaultPropertyHandler> defaultPropertyHandlers = new SparseArray<>();
+
+        SparseArray<MethodInfo> defaultProperties = allDefaultProperties.get(cl.hashCode());
+        if (defaultProperties == null) {
+            process(cl);
+            defaultProperties = allDefaultProperties.get(cl.hashCode());
+        }
+
+        int length = defaultProperties.size();
+        if (length != 0) {
+            for (int i = 0; i < length; i++) {
+                int key = defaultProperties.keyAt(i);
+
+                MethodInfo methodInfo = defaultProperties.valueAt(i);
+
+                DefaultPropertyHandler handler = new DefaultPropertyHandler(object, methodInfo);
+                defaultPropertyHandlers.put(key, handler);
+            }
+        }
+
+        return defaultPropertyHandlers;
+
+    }
+
+    public static SparseArray<List<PropertySubscriberHandler>> findPropertySubscribers(Object object) {
+        Class<?> cl = object.getClass();
+
+        SparseArray<List<PropertySubscriberHandler>> actionHookHandlers = new SparseArray<>();
+
+        SparseArray<List<MethodInfo>> propertySubscribers = allPropertySubscribers.get(cl.hashCode());
+        if (propertySubscribers == null) {
+            process(cl);
+            propertySubscribers = allPropertySubscribers.get(cl.hashCode());
+        }
+
+        int length = propertySubscribers.size();
+        if (length != 0) {
+            for (int i = 0; i < length; i++) {
+                int key = propertySubscribers.keyAt(i);
+
+                List<MethodInfo> methodInfos = propertySubscribers.valueAt(i);
+                List<PropertySubscriberHandler> handlers = new ArrayList<>();
+
+                for (MethodInfo methodInfo : methodInfos) {
+                    PropertySubscriberHandler handler = new PropertySubscriberHandler(object, methodInfo);
                     handlers.add(handler);
                 }
 
